cbb50b6ab859181d321d88f0d75f0fc90eb3aec6
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-16 00:01:16 +0900

    printf

---
+++ Chapter2/printf.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	printf("이민준\n");
+	printf("동작구 본동 48-9번지\n");
+	printf("01024164634\n");
+
+	return 0;
+
+
+}
+
+int main(void) {
+	printf("%d\n", 10);
+	printf("내키는 %d cm이다.\n", 180);
+	return 0;
+
+
+}
+
+
