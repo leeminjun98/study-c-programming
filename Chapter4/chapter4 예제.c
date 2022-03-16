99954d6d65b14a00c30bdc692d2ce5415dd032bd
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-16 23:41:46 +0900

    Chapter 4

---
+++ Chapter4/chapter4 예제.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+
+int main(void) {
+
+	int num1;
+
+
+	printf("아무수나입력하시오 : \n");
+	scanf_s("%d", &num1);
+
+	int num2 = ~num1;
+	printf("%d", ++num2);
+}
+
+
+
+
+
+int main(void) {
+
+	printf("3x8/4 \n");
+	
+	int num1 = 3;
+	int num2 = num1 << 3;
+	int num3 = num2 >> 2;
+
+	printf("%d", num3);
+
+
+}
