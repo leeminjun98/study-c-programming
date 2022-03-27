84620a4c5af692b1c3187f238f28431652d7a0c1
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-27 16:28:05 +0900

    Chapter 11-2 quiz

---
+++ Chapter11/Chapter11-2 문제.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	char arr[] = { 'G','o','o','d',' ','t','i','m','e' };
+	int arrLen = sizeof(arr) / sizeof(char);
+	int i;
+
+	for (i = 0; i < arrLen; i++)
+	{
+		printf("%c", arr[i]);
+
+	}
+	printf("\n");
+	return 0;
+
+}
