90f963f7886b0b89f09fc9b6b372f41b9f5e1825
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-29 20:29:40 +0900

    Chapter12-1문제

---
+++ Chapter12/Chapter12-1문제.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+
+//12-1-1
+int main(void) {
+	int num = 10;
+	int* ptr1 = &num;
+	int* ptr2 = ptr1;
+
+	(*ptr1)++;
+	(*ptr2)++;
+	printf("%d\n", num);
+	return 0;
+
+}
+
+//12-1-2
+int main(void) {
+	int num1 = 10, num2 = 20;
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+
+	*ptr1 = num1 + 10;
+	*ptr2 = num2 - 10;
+
+	int temp;
+	temp = num1;
+	*ptr1 = num2;
+	*ptr2 = temp;
+	
+	printf("num1: %d, num2: %d", *ptr1, *ptr2);
+
+}
