c46b18ad697f73a39340dc8e17ad2de6039c7f9a
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-30 21:41:26 +0900

    Chapter 13 예제

---
+++ Chapter13/Chapter13 예제.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int arr1[3] = { 1,2,3 };
+	double arr2[3] = { 1.1, 2.2, 3.3 };
+
+	printf("%d, %g \n", *arr1, *arr2);
+	*arr1 += 100;
+	*arr2 += 120.5;
+	printf("%d %g \n", arr1[0], arr2[0]);
+	return 0;
+}
+
+
+int main(void) {
+	int arr[3] = { 15,25,35 };
+	int* ptr = &arr[0];
+
+	printf("%d %d \n", ptr[0], arr[0]);
+	printf("%d %d \n", ptr[1], arr[1]);
+	printf("%d %d \n", ptr[2], arr[2]);
+	printf("%d %d \n", *ptr, *arr);
+	return 0;
+
+}
+
+
+
+int main(void) {
+	int arr[3] = { 11,22,33 };
+	int* ptr = arr;
+	printf("%d %d %d \n", *ptr, *(ptr + 1), *(ptr + 2));
+
+	printf("%d ", *ptr); ptr++;
+	printf("%d ", *ptr); ptr++;
+	printf("%d ", *ptr); ptr--;
+	printf("%d ", *ptr); ptr--;
+	printf("%d ", *ptr); printf("\n");
+	return 0;
+
+}
+
