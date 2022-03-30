017bf07ec34e0f8b54835c424039e91a96231ccd
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-30 23:02:10 +0900

    Update Chapter13 예제.c

--- Chapter13/Chapter13 예제.c
+++ Chapter13/Chapter13 예제.c
@@ -41,3 +41,12 @@ int main(void) {
 
 }
 
+int main(void) {
+
+	char* strArr[3] = { "Simple", "String", "Array" };
+	printf("%s \n", strArr[0]);
+	printf("%s \n", strArr[1]);
+	printf("%s \n", strArr[2]);
+	return 0;
+
+}
