7ba12da3f2b282a265ab94549bea353f55bd3997
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2022-03-31 23:10:31 +0900

    Chapter14문제

---
+++ Chapter14/Chapter14문제.c
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+
+//Chapter 14-1-1, Call-by-value & Call-by-reference
+void SquareByValue(int x) {
+	
+	x = x * x;
+	printf("%d\n", x);
+}
+
+void SquareByReference(int* p) {
+	
+	p[0] = p[0] * p[0];
+	printf("%d", p[0]);
+}
+
+int main(void) {
+	int num;
+	scanf_s("%d", &num);
+	SquareByValue(3);
+	SquareByReference(&num);
+
+
+}
+
+
+//Chapter 14-1-2
+void Swap3(int* a, int* b, int* c) {
+	int temp = *c;
+
+	*c = *b;
+	*b = *a;
+	*a = temp;
+
+
+}
+
+int main(void) {
+
+	int num1, num2, num3;
+	scanf_s("%d %d %d", &num1, &num2, &num3);
+	Swap3(&num1, &num2, &num3);
+	printf("%d %d %d", num1, num2, num3);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
